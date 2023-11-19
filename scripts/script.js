a93db49b7d884dbdbda323bc3c0af1690a0b9dc2
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-11-19 18:44:56 +0300

    added moove

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,93 @@
 const square = document.querySelector(".square-body");
-const blocks = square.querySelectorAll(".block");
+const resBtn = document.querySelector(".btn-reset");
+let blocks = square.querySelectorAll(".block");
+
+let arrBlocks = [];
+
+const rewrite = function () {
+  blocks = square.querySelectorAll(".block");
+
+  blocks.forEach((block, index) => {
+    block.id = `${index}`;
+  });
+};
+
+const moveLeft = function (block) {
+  let numberBlock = parseInt(block.id);
+  for (let i = 0; i < blocks.length - 1; i++) {
+    if (numberBlock === i && i > 0) {
+      square.insertBefore(block, blocks[i - 1]);
+      rewrite();
+    }
+  }
+};
+
+const moveRight = function (block) {
+  let numberBlock = parseInt(block.id);
+  for (let i = 0; i < blocks.length - 1; i++) {
+    if (numberBlock === i && i <= 25) {
+      square.insertBefore(block, blocks[i + 2]);
+      rewrite();
+    }
+  }
+};
+
+const moveTop = function (block) {
+  let numberBlock = parseInt(block.id);
+  for (let i = 0; i < blocks.length - 1; i++) {
+    if (numberBlock === i && i >= 5) {
+      let newBlock = blocks[i - 5];
+      square.insertBefore(block, blocks[i - 5]);
+      square.insertBefore(newBlock, blocks[i + 1]);
+      rewrite();
+    }
+  }
+};
+
+const moveBottom = function (block) {
+  let numberBlock = parseInt(block.id);
+  for (let i = 0; i < blocks.length - 1; i++) {
+    if (numberBlock === i && i < 20) {
+      let newBlock = blocks[i + 5];
+      console.log(newBlock);
+      square.insertBefore(block, blocks[i + 6]);
+      square.insertBefore(newBlock, blocks[i + 1]);
+      rewrite();
+    }
+  }
+};
+
+const reset = function () {
+  blocks = square.querySelectorAll(".block");
+
+  blocks.forEach((block) => {
+    blockText = block.querySelector(".block-number");
+    console.log(block, blockText);
+  });
+};
 
 square.addEventListener("click", (e) => {
-  blocks.forEach((block, index) => {
-    let button = block.querySelector(".block-btn");
-    let arrows = block.querySelectorAll(".block-btn>.arrow");
-
+  blocks.forEach((block) => {
     let arrowLeft = block.querySelector(".block-btn>.left>img");
     let arrowRigth = block.querySelector(".block-btn>.right>img");
     let arrowTop = block.querySelector(".block-btn>.top>img");
     let arrowBottom = block.querySelector(".block-btn>.bottom>img");
+    let blockBtn = block.querySelector(".block-btn");
 
     if (e.target === arrowLeft) {
-      console.log(block, index, "влево");
+      moveLeft(block);
     } else if (e.target === arrowRigth) {
-      console.log(block, index, "вправо");
+      moveRight(block);
     } else if (e.target === arrowTop) {
-      console.log(block, index, "вверх");
+      moveTop(block);
     } else if (e.target === arrowBottom) {
-      console.log(block, index, "вниз");
+      moveBottom(block);
+    } else if (e.target === blockBtn) {
+      return;
     }
   });
 });
+
+resBtn.addEventListener("click", reset);
+
+rewrite();
